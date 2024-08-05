ba1e047e8e316aa14e77ed92b8fd44ed61a6a180
Miv2nir <miv2nir@gmail.com>
2024-08-05 19:52:24 +0300

    player obj

---
+++ XYSt_util/players.py
@@ -0,0 +1,21 @@
+from XYSt_util import game
+from XYSt_util.names import Names,Space
+
+class Player:
+    def __init__(self,name:Names):
+        self.name=name
+    
+    def move(self,game_obj:game.Grid,x,y,verbal=False):
+        print(self.name.name,'sets a piece on (%s,%s)'%(x,y))
+        if self.name==Names.WHITE:
+            game_obj.put(x,y,Space.WHITE)
+        else:
+            game_obj.put(x,y,Space.BLACK)
+        return game_obj.evaluate()
+    
+    def analyze(self,game_obj:game.Grid,decision_max_seconds,move=True,verbal=False):
+        '''
+        Suggests the next most beneficial move to the player.
+        According to the design this function should only be called when the player is Black AKA a bot.
+        '''
+        #algo goes here, continue on from here later
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from XYSt_util import game,names
 
 def main():
     g=game.Grid()
-    g.put(5,5,names.Space.WHITE)
-    g.put(6,5,names.Space.WHITE)
-    g.put(7,5,names.Space.WHITE)
-    g.put(8,5,names.Space.WHITE)
-    g.put(9,5,names.Space.WHITE)
+    g.put(5,5,names.Space.BLACK)
+    g.put(6,6,names.Space.BLACK)
+    g.put(7,7,names.Space.BLACK)
+    g.put(8,8,names.Space.BLACK)
+    g.put(9,9,names.Space.BLACK)
     g.print_grid()
     g.evaluate()
 
